4b07910073e2b3a17cb159ab8c3565f73f821e11
tanmay1217 <52773110+tanmay1217@users.noreply.github.com>
2024-02-20 17:10:52 +0530

    Update 10. Prime  Factorization.cpp

--- Level-1/1. Basics of programming/10. Prime  Factorization.cpp
+++ Level-1/1. Basics of programming/10. Prime  Factorization.cpp
@@ -1,6 +1,7 @@
 //M1(by me)
 #include <iostream>
 using namespace std;
+//methodology-print krte jao divisor se jb tk rem 0 aa rha hai, vrna next divisor dhundho jo agla prime no. ho
 //func to chk if next generated prime no. or not 
 int chkprime(int num){
   int flag=1;
@@ -28,11 +29,42 @@ void fun(int n){
     }
     else{
       divisor = get_next_prime(divisor);
-
     }
   }
 }
+
 int main() 
 {
     fun(1440);
 }
+
+
+
+
+*****************************************************************************************************************************************************************
+
+  
+M2)
+#include <iostream>
+using namespace std;
+
+void printPrimeFactors(int n){
+  for(int i=2;i*i<=n;i++){
+    while(n%i == 0){
+      cout<<"factors is "<<i<<", ";
+      n/=i;
+      //cout<<"n= "<<n<<" ";
+    }
+  }
+  //kyoki agar koi no. root n tk divide hi divide ho skta hai, if at all wo pura divide hoga 1 nahi bna to wah no. wo khud hai
+  if(n!=1){
+    cout<<n;
+  }
+}
+int main() 
+{
+  printPrimeFactors(46);
+  return 0;
+}
+
+
