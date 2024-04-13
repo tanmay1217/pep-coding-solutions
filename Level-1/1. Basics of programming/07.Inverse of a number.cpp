bd864dc5b48a1728aa3c8562b408975b9257e027
tanmay1217 <52773110+tanmay1217@users.noreply.github.com>
2024-04-13 12:19:08 +0530

    Update 07.Inverse of a number.cpp

--- Level-1/1. Basics of programming/07.Inverse of a number.cpp
+++ Level-1/1. Basics of programming/07.Inverse of a number.cpp
@@ -1,39 +1,41 @@
 #include <bits/stdc++.h>
-#include<cmath>
 using namespace std;
-
-#function to check the provided constraints
-int chk_constraints(int n,int num){
-  int flag=1;
-  int temp=num;
-  while(temp){
-    int last = temp%10;
-    temp/=10;
-    if(last>n){ 
-      flag = 0;
-    }
-  }
-  return flag;
-}
-
-#
-void fun(int n,int num){
-  if(!chk_constraints(n,num)){
-    return;
-  }
-  int temp=num;
-  int idx=1;
+int chk_constraints(int num){
+  //now sort no in dec order and compare from original one 
+  int temp = num;
+  int flag =1;
   int sum=0;
   while(temp){
-    int last = temp%10;
+    int last_digit = temp%10;
+    sum+=last_digit*pow(10,last_digit-1);
     temp/=10;
-    sum+=idx*pow(10,last-1);
-    idx++;
   }
-  cout<<sum;
-} 
+  if(ceil(log10(sum)) != ceil(log10(num))){
+    cout<<"Error in i/p...";
+    flag=0;
+  }
+  //cout<<sum;
+  return flag;
+}
+void fun(int n){
+  cout<<"i/p:\t"<<n<<endl;
+  if(!chk_constraints(n)){
+    return;
+  }
+  //if passed from constraint_chk then,
+  int temp=n;
+  int idx_from_leftSide=1;
+  int sum=0;
+  while(temp){
+    int last_digit = temp%10;
+    sum+=idx_from_leftSide*pow(10,last_digit-1);
+    idx_from_leftSide++;
+    temp/=10;
+  }
+  cout<<"o/p:\t"<<sum;
+}
 int main() 
 {
-    fun(5,21453);
+    fun(12234);
     return 0;
 }
