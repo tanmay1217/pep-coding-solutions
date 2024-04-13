16d0e58c133e4d4337788c8ee538a4c532a6d3bc
tanmay1217 <52773110+tanmay1217@users.noreply.github.com>
2024-04-13 19:48:16 +0530

    Create 03.AnyBase_to_Decimal.cpp

---
+++ Level-1/2.Function & Arrays/03.AnyBase_to_Decimal.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void givenBase_to_decimal(int n,int base){
+  int pow_count=0;
+  int sum=0;
+  while(n){
+    int rem = n%10;
+    sum+= rem* pow(8,pow_count);
+    pow_count++;
+    n/=10;
+  } 
+  cout<<sum;
+}
+int main() 
+{
+    givenBase_to_decimal(1266,8);
+    return 0;
+}
