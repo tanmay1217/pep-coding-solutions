ecbe7c9fe756083bed83ffbe1f0bc3c0fae2946d
tanmay1217 <52773110+tanmay1217@users.noreply.github.com>
2024-04-15 15:49:44 +0530

    Update AnyBaseAddition.cpp

--- Level-1/2.Function & Arrays/AnyBaseAddition.cpp
+++ Level-1/2.Function & Arrays/AnyBaseAddition.cpp
@@ -1,4 +1,4 @@
-M1)by me
+//M1)by me
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
@@ -27,6 +27,49 @@ int main() {
     int base=5;
     int n1=234;
     int n2=343;
+    addition(base,n1,n2);
+    return 0;
+}
+
+
+
+
+
+
+//m2-by me
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void addition(int base,int n1,int n2){
+    //approach:extract last digit of both numbers
+    //add them generally and then mod them by their base
+    //we get quotient and remiander respectively
+    //use quotient int the next tens/hundreds and place rem as it is in ans,
+    //and correspondingly increse power of unit digit...
+    int greater = n1>n2 ? n1:n2;
+    int smaller = n1<n2 ? n1:n2;
+    int sum=0,pow_count=0;
+    while(greater){
+        int ld_g = greater%10;
+        int ld_s = smaller%10;
+        int div = (ld_g + ld_s)/base;
+        int rem = (ld_g + ld_s)%base;
+        sum+=rem*pow(10,pow_count);
+        pow_count++;
+        if(div){
+            sum+=div*pow(10,pow_count);
+        }
+        
+        greater/=10;
+        smaller/=10;
+    }
+    cout<<sum;
+}
+int main() {
+    int base=8;
+    int n1=767;
+    int n2=421;
     addition(base,n1,n2);
     return 0;
 }
